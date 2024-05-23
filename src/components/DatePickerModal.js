8447ec2e009dd61a4bd43271db9e0829f921eb8f
hyuk <hyuk.kang1@cj.net>
2024-05-23 19:01:19 +0900

    pacakge.json 정리 / DatePickerModal 테스트 컴포넌트 추가

--- src/components/DatePickerModal.js
+++ src/components/DatePickerModal.js
@@ -1,23 +1,3 @@
-import React, { useState } from "react";
-// import DatePicker from "react-native-date-picker";
-
-export default function DatePickerModal({ open, onOpen }) {
-  const [date, setDate] = useState(new Date());
-
-  return (
-    <>
-      {/* <DatePicker
-        modal
-        open={open}
-        date={date}
-        onConfirm={(date) => {
-          onOpen(false);
-          setDate(date);
-        }}
-        onCancel={() => {
-          onOpen(false);
-        }}
-      /> */}
-    </>
-  );
+export default function DatePickerModal() {
+  return <></>;
 }
