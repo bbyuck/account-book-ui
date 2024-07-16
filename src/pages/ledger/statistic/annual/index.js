084239e027ebb7cd7f1fd0bb415de5ded26764ac
hyuk <k941026h@naver.com>
2024-07-17 02:14:00 +0900

    월 단위 기간 통계 조회 API 호출 샘플 코드 적용

--- src/pages/ledger/statistic/annual/index.js
+++ src/pages/ledger/statistic/annual/index.js
@@ -1,10 +1,28 @@
+import api from "api";
 import HeaderBackButton from "components/header/back-button";
 import Page from "components/page";
+import { useEffect } from "react";
 
 export default function LedgerAnnualStatistic() {
   const headerInfo = {
     left: <HeaderBackButton />,
   };
+
+  useEffect(() => {
+    const params = {
+      startYear: 2024,
+      startMonth: 1,
+      endYear: 2024,
+      endMonth: 12,
+    };
+
+    api
+      .get("/api/v1/ledger/statistic/period/categorization", { params })
+      .then((response) => {
+        console.log(response.data.data);
+      })
+      .catch((error) => {});
+  }, []);
 
   return <Page headerInfo={headerInfo}></Page>;
 }
